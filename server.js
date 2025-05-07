c6e56321bb68a6544b762bb273d04fec7cd06f8d
Maxgit3 <52330733+Maxgit3@users.noreply.github.com>
2025-05-07 06:28:07 +0000

    initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
@@ -10,13 +11,36 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+// bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+//     console.log(hash);
+//     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+//     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+//       console.log(res); //true
+//     });
+//   });
 
+  async function bcryptHash() {
+     await bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+        console.log(hash)
+        // return hash  
+        bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+            console.log(res)
+            // return res
+        })
+        
+    })
+    
 
+  }
 
+  bcryptHash();
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
